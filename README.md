4f481ea40a063647898592f0f4bb602f6bb33938
JoelCourtney <joel.e.courtney@gmail.com>
2022-04-22 10:50:57 -0600

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # mdbook-kroki-preprocessor
-Render Kroki diagrams from files or code blocks in mdbook
+
+An mdbook preprocessor that lets you embed diagrams from any of [Kroki's](https://kroki.io)
+endpoints into your book.
+
+## Setup
+
+First install this preprocessor with `cargo install mdbook-kroki-preprocessor`.
+
+Then add the preprocessor to your `book.toml`:
+
+```toml
+[book]
+authors = ["You"]
+language = "en"
+multilingual = false
+src = "src"
+title = "example"
+
+[preprocessor.kroki-preprocessor]
+```
+
+## Usage
+
+There are two ways to use Kroki in your book. First is a fenced code block:
+
+``````markdown
+```kroki-mermaid
+graph TD
+  A[ Anyone ] -->|Can help | B( Go to github.com/yuzutech/kroki )
+  B --> C{ How to contribute? }
+  C --> D[ Reporting bugs ]
+  C --> E[ Sharing ideas ]
+  C --> F[ Advocating ]
+```
+``````
+
+The code block's language has to be `kroki-<diagram type>`.
+
+The other method is to use an image tag, for diagrams contents that are too big to put inline
+in the markdown (such as for excalidraw):
+
+```markdown
+![Excalidraw example](kroki-excalidraw:example.excalidraw)
+```
+
+The title field can be anything, but the source field needs to start with `kroki-<diagram type>:`.
+Both relative and absolute paths are supported. Relative paths are relative to the current markdown
+source file, *not* the root of the mdbook.
+
+The preprocessor will collect all Kroki diagrams of both types, send requests out in parallel
+to the appropriate Kroki API endpoint, and replace their SVG contents back into the markdown.
+
+## Other
+
+This preprocessor only supports HTML rendering.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,57 @@
 //! An mdbook preprocessor that lets you embed diagrams from any of [Kroki's](https://kroki.io)
 //! endpoints into your book.
+//! 
+//! # Setup
+//! 
+//! First install this preprocessor with `cargo install mdbook-kroki-preprocessor`.
+//! 
+//! Then add the preprocessor to your `book.toml`:
+//! 
+//! ```toml
+//! [book]
+//! authors = ["You"]
+//! language = "en"
+//! multilingual = false
+//! src = "src"
+//! title = "example"
+//!
+//! [preprocessor.kroki-preprocessor]
+//! ```
+//! 
+//! # Usage
+//! 
+//! There are two ways to use Kroki in your book. First is a fenced code block:
+//! 
+//! ``````markdown
+//! ```kroki-mermaid
+//! graph TD
+//!   A[ Anyone ] -->|Can help | B( Go to github.com/yuzutech/kroki )
+//!   B --> C{ How to contribute? }
+//!   C --> D[ Reporting bugs ]
+//!   C --> E[ Sharing ideas ]
+//!   C --> F[ Advocating ]
+//! ```
+//! ``````
+//! 
+//! The code block's language has to be `kroki-<diagram type>`.
+//! 
+//! The other method is to use an image tag, for diagrams contents that are too big to put inline
+//! in the markdown (such as for excalidraw):
+//! 
+//! ```markdown
+//! ![Excalidraw example](kroki-excalidraw:example.excalidraw)
+//! ```
+//! 
+//! The title field can be anything, but the source field needs to start with `kroki-<diagram type>:`.
+//! Both relative and absolute paths are supported. Relative paths are relative to the current markdown
+//! source file, *not* the root of the mdbook.
+//! 
+//! The preprocessor will collect all Kroki diagrams of both types, send requests out in parallel
+//! to the appropriate Kroki API endpoint, and replace their SVG contents back into the markdown.
+//! 
+//! # Other
+//! 
+//! This preprocessor only supports HTML rendering.
 
 use anyhow::{Result, anyhow, bail, Context};
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
@@ -52,6 +104,9 @@ impl Preprocessor for KrokiPreprocessor {
     }
 }
 
+/// Recursively scans all chapters for diagrams.
+/// 
+/// Uses `parse_and_replace` to pull out the diagrams.
 fn extract_diagrams<'a>(items: impl IntoIterator<Item=&'a mut BookItem> + 'a, indices: &mut Vec<usize>) -> Result<Vec<Diagram>> {
     let mut diagrams = Vec::new();
     indices.push(0);
@@ -148,6 +203,9 @@ struct KrokiRequestBody {
     output_format: &'static str
 }
 
+/// Listens on the cmark pulldown parser and replaces kroki diagrams
+/// in the text with "%%kroki-diagram-N%%", which will be replaced again
+/// later when the diagram is rendered.
 fn parse_and_replace(chapter: &mut Chapter, indices: &Vec<usize>) -> Result<Vec<Diagram>> {
     let text = &mut chapter.content;
 
