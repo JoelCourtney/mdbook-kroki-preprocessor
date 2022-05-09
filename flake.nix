b0993d2059accad6b4377cab289175ceca683ede
Robert Schardt <robert.schardt@iohk.io>
2022-05-09 15:41:18 +0200

    Add pkgs-config and openssl as inputs.

--- flake.nix
+++ flake.nix
@@ -12,13 +12,17 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+        defaultPackage = with pkgs; rustPlatform.buildRustPackage {
           pname = "mdbook-kroki-preprocessor";
           version = "0.1.0";
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [ openssl ];
 
           cargoLock = {
             lockFile = ./Cargo.lock;
           };
+
+          src = ./.;
       };
     });
 }
